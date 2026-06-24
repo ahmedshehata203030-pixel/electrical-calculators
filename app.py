dded45a29052a78eefc139a8cb7af6d1b630f355
ahmedshehata203030-pixel <ahmedshehata203030@gmail.com>
2026-06-24 18:33:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ with tab2:
 # التبويب الثالث: CIRCUIT BREAKER CALCULATION
 # ------------------------------------------------------------------
 with tab3:
-    st.header("🛡️ اختيار تيار القاطع الحامي (Circuit Breaker current)")
+    st.header("🛡️ اختيار تيار القاطع   (Circuit Breaker current)")
     
     u_input = st.number_input("القدرة بالكيلو فولت أمبير Power (KVA):", min_value=0.0, value=0.0, step=1.0, key="u_in")
     
